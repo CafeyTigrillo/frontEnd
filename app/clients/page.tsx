721deecb10d146d39905d4af19068b204657dc96
CafeyTigrillo <cafeytigrillomagic@gmail.com>
2025-02-14 03:28:05 -0500

    Update url clients

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -28,7 +28,7 @@ type Cliente = {
   email: string
 }
 
-const API_URL = "http://34.196.60.192:8081/clients"
+const API_URL = "http://ec2-13-216-183-248.compute-1.amazonaws.com:8081/clients"
 
 export default function GestionClientes() {
   const [clientes, setClientes] = useState<Cliente[]>([])
